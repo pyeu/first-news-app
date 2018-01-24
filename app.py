0887fe7befe61f74ee67dd8a5dc7ffb5f06379e9
pushpendrasharma <pushpendrasharma@hotmail.com>
2018-01-24 18:09:55 +0530

    Made a map on the detail page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import csv
 from flask import Flask
+from flask import abort
 from flask import render_template
 app = Flask(__name__)
 
@@ -15,6 +16,15 @@ def index():
 	template = 'index.html'
 	object_list = get_csv()
 	return render_template(template, object_list=object_list)
+
+@app.route('/<row_id>/')
+def detail(row_id):
+		template = 'detail.html'
+		object_list = get_csv()
+		for row in object_list:
+			if row['id'] == row_id:
+				return render_template(template, object=row)
+			abort(404)	
 
 
 
